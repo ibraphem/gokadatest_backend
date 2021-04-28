45482e7aee0e3748f67c9f27f5d55fde9533e5dc
ibraphem <ibraphem@gmail.com>
2021-04-28 21:44:15 +0100

    first commit

---
+++ database/migrations/2021_04_28_190041_create_locations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->float('latitude', 9, 8);
+            $table->float('longitude', 9, 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
